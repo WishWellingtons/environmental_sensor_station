c127b4314ab1af428a3a86f7630ac2f3a062faea
WishWellingtons <165937970+WishWellingtons@users.noreply.github.com>
2025-06-25 12:31:05 +0100

    Update envSensor station.cpp
    added license header

--- envSensor station.cpp
+++ envSensor station.cpp
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Will M (WishWellingtons)
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
 //envSensor station
 
 #include <time.h>
